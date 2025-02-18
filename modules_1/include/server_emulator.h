6a768c25d7d05a5d041b47379abe6cd51b67a2f3
Shkrest OS <telephon.inet@gmail.com>
2025-02-18 23:47:51 +0300

    modules 1 reverc rev 3

--- modules_1/include/server_emulator.h
+++ modules_1/include/server_emulator.h
@@ -3,38 +3,25 @@
 
 #include <string>
 #include <asio.hpp>
-#include "utils.h"
+#include "utils.h" // Добавляем utils.h
 
 class ServerEmulator {
 public:
-    // Конструктор
     ServerEmulator(const std::string& ipAddrParam, int portNumParam, asio::io_context& ioContextParam, const Config& configParam);
-
-    // Деструктор
     ~ServerEmulator();
 
-    // Метод для прослушивания подключений
     void listenForConnections();
-
-    // Метод для отправки информации на мастер-сервер
-    void sendMasterServerInfo();
-
-    // Проверка, привязан ли сервер к порту
     bool isBound() const;
 
 private:
+    asio::ip::udp::socket socket; // Сокет должен быть инициализирован раньше
+    asio::io_context& ioContext;
+    Config config; // Конфигурация сервера
     std::string ip;
     int port;
     bool bound = false;
-    Config config;
-    asio::io_context& ioContext;
-    asio::ip::udp::socket socket;
 
-    // Метод для создания и отправки пакета A2S_INFO
-    void handleA2SInfoRequest(asio::ip::udp::endpoint senderEndpoint);
-
-    // Метод для создания и отправки пакета A2S_PLAYER
-    void handleA2SPlayerRequest(asio::ip::udp::endpoint senderEndpoint);
+    void sendMasterServerInfo();
 };
 
 #endif // SERVER_EMULATOR_H
